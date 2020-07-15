ba8497981235eb811e71174397f86d03ae9e43d6
Joseph Haaga <jhaaga@interos.ai>
2020-07-15 09:38:05 -0400

    update journal.sh script

--- scripts/journal.sh
+++ scripts/journal.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 today=$(date +"%Y-%m-%d")
-FILE="$HOME/Documents/Journal/$today.md"
+FILE="$HOME/Documents/Journal/journals/$today.md"
 
 if [[ ! -f "$FILE" ]]; then
     echo "Creating new journal entry for $today"
     touch $FILE
-    echo "# $(date)" >> file
+    echo "# $(date)" >> $FILE
 fi
 
 vi $FILE

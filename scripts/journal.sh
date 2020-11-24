cedd7f810f521dbc916c5b43646d58ab2a39eeab
Joseph Haaga <haaga.joe@gmail.com>
2020-11-24 13:20:28 -0500

    update journal to add self-care block

--- scripts/journal.sh
+++ scripts/journal.sh
@@ -7,6 +7,15 @@ if [[ ! -f "$FILE" ]]; then
     echo "Creating new journal entry for $today"
     touch $FILE
     echo "# $(date)" >> $FILE
+    echo "" >> $FILE
+    echo "" >> $FILE
+    echo "## Self Care" >> $FILE
+    echo "Physical: " >> $FILE
+    echo "Psychological" >> $FILE
+    echo "Social: " >> $FILE
+    echo "Spiritual: " >> $FILE
+    echo "Professional: " >> $FILE
+    echo "" >> $FILE
 fi
 
 vi $FILE

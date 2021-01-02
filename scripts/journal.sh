0ec41b70f3a9b1364bf6a9ec91d95e21d6e13e01
Joseph Haaga <haaga.joe@gmail.com>
2021-01-02 10:03:34 -0500

    remove self care block

--- scripts/journal.sh
+++ scripts/journal.sh
@@ -8,14 +8,14 @@ if [[ ! -f "$FILE" ]]; then
     touch $FILE
     echo "# $(date)" >> $FILE
     echo "" >> $FILE
-    echo "" >> $FILE
-    echo "## Self Care" >> $FILE
-    echo "Physical: " >> $FILE
-    echo "Psychological" >> $FILE
-    echo "Social: " >> $FILE
-    echo "Spiritual: " >> $FILE
-    echo "Professional: " >> $FILE
-    echo "" >> $FILE
+#   echo "" >> $FILE
+#   echo "## Self Care" >> $FILE
+#   echo "Physical: " >> $FILE
+#   echo "Psychological" >> $FILE
+#   echo "Social: " >> $FILE
+#   echo "Spiritual: " >> $FILE
+#   echo "Professional: " >> $FILE
+#   echo "" >> $FILE
 fi
 
 vi $FILE

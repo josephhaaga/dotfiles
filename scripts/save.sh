c30fc04bcb358c05237d66b30a345df2323b2e61
Joseph Haaga <haaga.joe@gmail.com>
2022-12-05 09:45:57 -0500

    Autosave at Mon Dec  5 09:45:56 EST 2022

--- scripts/save.sh
+++ scripts/save.sh
@@ -5,11 +5,11 @@ COMMIT_MESSAGE="Autosave at $(date)"
 echo $COMMIT_MESSAGE
 
 git -C ~/Documents/Journal add .
-git -C ~/Documents/Journal commit -m '$COMMIT_MESSAGE'
+git -C ~/Documents/Journal commit -m "${COMMIT_MESSAGE}"
 git -C ~/Documents/Journal push
 
 git -C ~/Documents/dotfiles add .
-git -C ~/Documents/dotfiles commit -m '$COMMIT_MESSAGE'
+git -C ~/Documents/dotfiles commit -m "${COMMIT_MESSAGE}"
 git -C ~/Documents/dotfiles push
 
 

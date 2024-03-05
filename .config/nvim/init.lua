9e97a3ef5074dcc98d66c69fdacd13172c18669f
Joseph Haaga <haaga.joe@gmail.com>
2024-03-04 21:10:11 -0500

    add ChatGPT.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -815,6 +815,22 @@ require('lazy').setup {
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information see: :help lazy.nvim-lazy.nvim-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  -- {
+  --   'danth/pathfinder.vim',
+  -- },
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('chatgpt').setup()
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`

d8a96f21f3824f00a836db8b0f25faa99a660bbd
Joseph Haaga <haaga.joe@gmail.com>
2024-03-12 10:22:01 -0400

    nvim: format Python files on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -585,7 +585,9 @@ require('lazy').setup {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
-        'pyright',
+        'pyright', -- Python LSP
+        'black', -- Python formatter
+        'isort', -- Python formatter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -615,7 +617,7 @@ require('lazy').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.

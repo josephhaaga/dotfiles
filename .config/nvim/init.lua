5f5239ed75bd3912f53559516e6a0c8993be3b15
Joseph Haaga <haaga.joe@gmail.com>
2024-03-26 10:00:58 -0400

    nvim: make treesitter install markdown_inline parser

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -778,7 +778,7 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'markdown_inline', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },

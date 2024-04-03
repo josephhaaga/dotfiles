a436a34930449b1a94f2c85d897e81362d921a61
Joseph Haaga <haaga.joe@gmail.com>
2024-04-03 16:12:42 -0400

    nvim: example dap config for containers

--- .config/nvim/lua/custom/commands.lua
+++ .config/nvim/lua/custom/commands.lua
@@ -18,4 +18,36 @@ M.usepython = function()
 	end)
 end
 
+-- https://github.com/mfussenegger/nvim-dap/wiki/Local-and-Remote-Debugging-with-Docker#python
+M.attach_python_debugger = function(args)
+	local dap = require 'dap'
+	local host = args[1] -- This should be configured for remote debugging if your SSH tunnel is setup.
+	local port = 5678
+
+	-- You can even make nvim responsible for starting the debugpy server/adapter:
+	--  vim.fn.system({"${some_script_that_starts_debugpy_in_your_container}", ${script_args}})
+	pythonAttachConfig = {
+		type = 'python',
+		request = 'attach',
+		connect = {
+			port = port,
+			host = host,
+		},
+		mode = 'remote',
+		name = 'Remote Attached Debugger',
+		cwd = vim.fn.getcwd(),
+		pathMappings = {
+			{
+				localRoot = vim.fn.getcwd(), -- Wherever your Python code lives locally.
+				remoteRoot = '/usr/src/app', -- Wherever your Python code lives in the container.
+			},
+		},
+	}
+	local session = dap.attach(host, port, pythonAttachConfig)
+	if session == nil then
+		io.write 'Error launching adapter'
+	end
+	dap.repl.open()
+end
+
 return M

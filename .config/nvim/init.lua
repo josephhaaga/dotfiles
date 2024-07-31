b0161429451df0f662d25a88f6ab355de1771b2c
Joseph Haaga <haaga.joe@gmail.com>
2024-07-31 12:37:57 -0700

    fix error thrown every first save of a buffer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -608,7 +608,7 @@ require('lazy').setup {
       notify_on_error = true,
       format_on_save = {
         timeout_ms = 500,
-        async = true,
+        -- async = true,
         lsp_fallback = 'true',
       },
       formatters_by_ft = {

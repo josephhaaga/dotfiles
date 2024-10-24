caf473ffbc49d6cd0281ad24e00131f71d5c9f57
Joseph Haaga <haaga.joe@gmail.com>
2024-10-24 11:18:29 -0400

    nvim: add commands to disable format-on-save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -644,11 +644,13 @@ require('lazy').setup {
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = true,
-      format_on_save = {
-        timeout_ms = 500,
-        -- async = true,
-        lsp_fallback = 'true',
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = 'fallback' } --         lsp_fallback = 'true',
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially

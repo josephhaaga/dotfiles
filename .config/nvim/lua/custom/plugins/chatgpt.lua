3f3d5b1cf23e8e629f22562b061f73653fc8d82e
Joseph Haaga <haaga.joe@gmail.com>
2025-05-20 16:07:11 -0400

    Autosave at Tue May 20 16:07:11 EDT 2025

--- .config/nvim/lua/custom/plugins/chatgpt.lua
+++ .config/nvim/lua/custom/plugins/chatgpt.lua
@@ -22,14 +22,9 @@ return {
     opts = {
       -- add any opts here
       -- for example
-      provider = 'openai',
-      openai = {
-        endpoint = 'https://api.openai.com/v1',
-        model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      provider = 'ollama',
+      ollama = {
+        model = 'qwen2.5-coder:3b',
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`

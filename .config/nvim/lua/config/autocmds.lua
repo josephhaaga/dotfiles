51a0b5e5bbf6e177cc0dc2d02aa387517f55ad72
Joseph Haaga <haaga.joe@gmail.com>
2025-06-15 21:03:30 -0400

    switch to LazyVim

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,16 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_user_command("Ts", function()
+  local datetime = os.date("%Y-%m-%d %I:%M:%S %p")
+  vim.api.nvim_feedkeys("i[" .. datetime .. "]\n", "n", true)
+end, {
+  desc = "Insert current datetime",
+  -- You can add other options here, e.g., `range = true` if it should operate on a range
+})

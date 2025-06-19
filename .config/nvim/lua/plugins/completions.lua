c3d7bac53955fd59ad04c74d61fbb8be9f1ce3d3
Joseph Haaga <haaga.joe@gmail.com>
2025-06-18 20:09:44 -0400

    use kickstart.nvim keymaps for completions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -2,6 +2,15 @@ return {
   {
     "saghen/blink.cmp",
     enabled = true,
-    opts = { completion = { trigger = { show_on_keyword = false } } },
+    opts = {
+      completion = {
+        accept = { auto_brackets = { enabled = false } },
+        trigger = { show_on_keyword = false },
+        menu = { auto_show = false },
+      },
+      keymap = {
+        ["<C-p>"] = { "show", "select_prev" }, -- TODO: add to which-key
+      },
+    },
   },
 }

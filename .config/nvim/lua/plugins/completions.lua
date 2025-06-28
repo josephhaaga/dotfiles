e7c7fbead1fe8e3061b24df732170c7741f3ae94
Joseph Haaga <haaga.joe@gmail.com>
2025-06-28 15:09:30 -0400

    nvim: try out supertab

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,16 +1,60 @@
+-- return {
+--   {
+--     "saghen/blink.cmp",
+--     enabled = true,
+--     opts = {
+--       completion = {
+--         accept = { auto_brackets = { enabled = false } },
+--         trigger = { show_on_keyword = false },
+--         menu = { auto_show = false },
+--       },
+--       keymap = {
+--         ["<C-p>"] = { "show", "select_prev" }, -- TODO: add to which-key
+--       },
+--     },
+--   },
+-- }
+
 return {
   {
-    "saghen/blink.cmp",
-    enabled = true,
-    opts = {
-      completion = {
-        accept = { auto_brackets = { enabled = false } },
-        trigger = { show_on_keyword = false },
-        menu = { auto_show = false },
-      },
-      keymap = {
-        ["<C-p>"] = { "show", "select_prev" }, -- TODO: add to which-key
-      },
-    },
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
   },
 }

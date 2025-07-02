578fec08ea9e5323f7fea7f9b23f1cad3be46f8a
Joseph Haaga <haaga.joe@gmail.com>
2025-07-02 17:37:33 -0400

    Autosave at Wed Jul  2 17:37:33 EDT 2025

--- scripts/save.sh
+++ scripts/save.sh
@@ -4,19 +4,22 @@ COMMIT_MESSAGE="Autosave at $(date)"
 repos=("$HOME/Documents/journal" "$HOME/Documents/dotfiles")
 
 for repo in "${repos[@]}"; do
-  echo "Saving changes in $repo"
-  if git -C "$repo" diff --quiet && git -C "$repo" diff --staged --quiet && git -C "$repo" rev-parse --verify HEAD; then
-    echo "No changes to save in $repo. Skipping."
+  echo "[$repo]"
+
+  changes=$(git -C "$repo" status --porcelain)
+  if [ -z "$changes" ]; then
+    echo "No changes to save. Skipping."
     continue
   fi
 
-  echo "$COMMIT_MESSAGE"
+  diffstat=$(git -C "$repo" diff --stat HEAD)
+  echo "$diffstat"
+
   if git -C "$repo" add . && git -C "$repo" commit -m "${COMMIT_MESSAGE}" && git -C "$repo" push; then
-    echo "$repo changes saved and pushed successfully."
+    :
   else
-    echo "Failed to save changes in $repo. Please check the error." >&2
     exit 1
   fi
+  echo
 
 done
-

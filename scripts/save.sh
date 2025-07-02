0c47aceca343c3756a196e17cc5c4a2b3985dca1
Joseph Haaga <haaga.joe@gmail.com>
2025-07-02 19:40:36 -0400

    Autosave at Wed Jul  2 19:40:35 EDT 2025

--- scripts/save.sh
+++ scripts/save.sh
@@ -4,17 +4,17 @@ COMMIT_MESSAGE="Autosave at $(date)"
 repos=("$HOME/Documents/journal" "$HOME/Documents/dotfiles")
 
 for repo in "${repos[@]}"; do
-  # gum style --foreground 212 "$repo"
-  gum style --bold "$repo"
+  TITLE=$(gum style --foreground 212 --bold "$repo")
 
   changes=$(git -C "$repo" status --porcelain)
   if [ -z "$changes" ]; then
-    gum style --italic --margin "0 1" "No changes to save. Skipping."
+    MESSAGE=$(gum style --italic --margin "0 1" "No changes to save. Skipping.")
     continue
   fi
 
-  diffstat=$(git -C "$repo" diff --stat HEAD)
-  echo "$diffstat" | gum format -t code
+  MESSAGE=$(git -C "$repo" diff --stat HEAD | gum format -t code)
+
+  gum join --vertical "$TITLE" "$MESSAGE"
 
   if git -C "$repo" add . && git -C "$repo" commit -m "${COMMIT_MESSAGE}" && git -C "$repo" push; then
     :

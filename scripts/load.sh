bba8e004a3c66d14095e9cfcef0391f87f9e6dce
Joseph Haaga <haaga.joe@gmail.com>
2025-07-04 12:07:29 -0400

    blah

--- scripts/load.sh
+++ scripts/load.sh
@@ -4,13 +4,13 @@
 repos=("$HOME/Documents/journal" "$HOME/Documents/dotfiles")
 
 for repo in "${repos[@]}"; do
-  echo "Updating $repo"
-  if git -C "$repo" pull; then
-    echo "$repo updated successfully."
+  TITLE=$(gum style --foreground 212 --bold "$repo")
+  PULL=$(git -C "$repo" pull >/dev/null 2>&1)
+  if [ "$PULL" = 0 ]; then
+    MESSAGE=$(gum style --italic --margin "0 1" "Pulled latest changes")
   else
     echo "Failed to update $repo. Please check the error." >&2
     exit 1
   fi
 
 done
-

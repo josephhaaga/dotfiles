b38e3df5ce5eade31de5f2126060c4d2af3279ce
Joseph Haaga <haaga.joe@gmail.com>
2025-07-08 16:40:22 -0400

    Autosave at Tue Jul  8 16:40:22 EDT 2025

--- scripts/save.sh
+++ scripts/save.sh
@@ -19,8 +19,9 @@ for repo in "${repos[@]}"; do
     continue
   fi
 
-  SAVE=$(git -C "$repo" add . >/dev/null 2>&1 && git -C "$repo" commit -m "${COMMIT_MESSAGE}" >/dev/null 2>&1 && git -C "$repo" push >/dev/null 2>&1)
-  if [ "$SAVE" = 0 ]; then
+  if git -C "$repo" add . >/dev/null 2>&1 &&
+    git -C "$repo" commit -m "${COMMIT_MESSAGE}" >/dev/null 2>&1 &&
+    git -C "$repo" push >/dev/null 2>&1; then
     :
   else
     echo "Failed to save $repo" >&2

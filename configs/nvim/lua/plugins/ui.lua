008f2c783446320293de43b3825494e3cc414d91
Joseph Haaga <haaga.joe@gmail.com>
2025-07-09 17:37:44 -0400

    lazyvim: replace `f` in Snacks Dashboard

--- configs/nvim/lua/plugins/ui.lua
+++ configs/nvim/lua/plugins/ui.lua
@@ -1,16 +1,13 @@
 return {
-  "snacks.nvim",
-  opts = {
-    dashboard = {
-      keys = {
-        {
-          icon = " ",
-          key = "f",
-          desc = "Find Project File",
-          -- action = ":lua Snacks.dashboard.pick('files', {cwd=vim.fn.getcwd()})",
-          action = ":lua Snacks.picker.files({cwd=vim.fn.getcwd()})",
-        },
-      },
-    },
-  },
+  "folke/snacks.nvim",
+  opts = function(_, opts)
+    table.remove(opts.dashboard.preset.keys, 1)
+    table.insert(opts.dashboard.preset.keys, 1, {
+      icon = " ",
+      key = "f",
+      desc = "Find Project File",
+      action = ":lua Snacks.picker.files({cwd=vim.fn.getcwd()})",
+      -- action = ":lua Snacks.dashboard.pick('files', {cwd=vim.fn.getcwd()})",
+    })
+  end,
 }

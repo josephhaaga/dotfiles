7894ecc09599c11867e6c894e3a039226070020f
Joseph Haaga <haaga.joe@gmail.com>
2025-08-28 12:22:58 -0400

    Autosave at Thu Aug 28 12:22:57 EDT 2025

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -7,4 +7,14 @@ return {
       colorscheme = "catppuccin-frappe",
     },
   },
+  {
+    -- https://github.com/LazyVim/LazyVim/pull/6354
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }

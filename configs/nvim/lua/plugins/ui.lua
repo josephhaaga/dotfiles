b8c9c5442cf3ff4796aad2fecb6c18784576c3cc
Joseph Haaga <haaga.joe@gmail.com>
2025-09-04 11:05:24 -0400

    nvim: disable annoying indent signs

--- configs/nvim/lua/plugins/ui.lua
+++ configs/nvim/lua/plugins/ui.lua
@@ -1,13 +1,15 @@
 return {
   "folke/snacks.nvim",
   opts = function(_, opts)
+    -- add "Find Project File" to dashboard commands
     table.remove(opts.dashboard.preset.keys, 1)
     table.insert(opts.dashboard.preset.keys, 1, {
       icon = " ",
       key = "f",
       desc = "Find Project File",
       action = ":lua Snacks.picker.files({cwd=vim.fn.getcwd()})",
-      -- action = ":lua Snacks.dashboard.pick('files', {cwd=vim.fn.getcwd()})",
     })
+    -- disable Indent Signs
+    opts.indent.enabled = false
   end,
 }

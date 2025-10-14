e4231b7434a21f9fb2230812ff3fb72382ce4cea
Joseph Haaga <haaga.joe@gmail.com>
2025-10-14 11:14:10 -0400

    Autosave at Tue Oct 14 11:14:09 EDT 2025

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -6,14 +6,14 @@ return {
       colorscheme = "tokyonight-moon", -- "catppuccin-frappe",
     },
   },
-  {
-    -- https://github.com/LazyVim/LazyVim/pull/6354
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
+  -- {
+  --   -- https://github.com/LazyVim/LazyVim/pull/6354
+  --   "akinsho/bufferline.nvim",
+  --   init = function()
+  --     local bufline = require("catppuccin.groups.integrations.bufferline")
+  --     function bufline.get()
+  --       return bufline.get_theme()
+  --     end
+  --   end,
+  -- },
 }

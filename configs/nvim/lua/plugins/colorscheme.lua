473b4ce4ca540c5ff18e3c23119c37156ac331dd
Joseph Haaga <haaga.joe@gmail.com>
2025-12-17 19:43:43 -0500

    Autosave at Wed Dec 17 19:43:42 EST 2025

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-moon", -- "catppuccin-frappe",
+      colorscheme = "tokyonight-storm", -- "catppuccin-frappe",
     },
   },
   -- {

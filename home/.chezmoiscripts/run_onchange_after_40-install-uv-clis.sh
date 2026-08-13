f8ef21e973af985bc757d0730ca2776cc70e32eb
Joseph Haaga <joseph.haaga@tribe.ai>
2026-08-13 19:26:28 -0400

    fix: install frogmouth with uv

--- home/.chezmoiscripts/run_onchange_after_40-install-uv-clis.sh
+++ home/.chezmoiscripts/run_onchange_after_40-install-uv-clis.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 eval "$(/opt/homebrew/bin/brew shellenv)"
 uv tool install --force git+https://github.com/josephhaaga/clerk.git
 uv tool install --force pre-commit
+uv tool install --force frogmouth
